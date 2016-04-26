58594f7353e80f31afcd44f52ca60433a106fabb
ahungrynacho <brianbhuynh@gmail.com>
2016-04-26 13:48:35 -0700

    Dim Sum Time

--- book_example.c
+++ book_example.c
@@ -1,11 +1,17 @@
-#include "csapp.h"
+//#include "csapp.h"
 #define MAXARGS 128
+#define MAXLINE 128
+#define N 2
 
 //unixerr.c
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+pid_t fork(void);
+int execvp(const char *filename, char *const argv[]);
+pid_t waitpid(pid_t pid, int *iptr, int options);
+
 
 void unix_error(char* msg) {
 	int errnum = errno;
@@ -21,6 +27,7 @@ int builtin_command(char **argv) {
 	return 0;
 }
 
+//splits a string into an array of arguments
 int parseline(char* buf, char **argv) {
 	char* delim;
 	int argc;
@@ -54,6 +61,7 @@ void eval(char* cmdline) {
 	char buf[MAXLINE];
 	int bg;
 	pid_t pid;
+	int status;
 	
 	strcpy(buf, cmdline);
 	bg = parseline(buf, argv);
@@ -62,7 +70,7 @@ void eval(char* cmdline) {
 		
 	if (!builtin_command(argv)) {
 		if ((pid = fork()) == 0) {
-			if (execve(argv[0], argv, environ) < 0) {
+			if (execvp(argv[0], argv) < 0) {
 				printf("%s: Command not found.\n", argv[0]);
 				exit(0);
 			}
@@ -76,16 +84,43 @@ void eval(char* cmdline) {
 		else
 			printf("%d %s", pid, cmdline);
 	}
+	
+	//reaping dead children only applies to background operations
+	//foreground operations do not produce children
+	while((pid = waitpid(-1, &status, 0)) > 0)
+	{
+		if(WIFEXITED(status))
+		{
+			printf("child %d terminated normally with exit status=%d\n", pid, WEXITSTATUS(status));
+		}
+		else
+		{
+			printf("child %d terminated abnormally\n", pid);
+		}
+	}
+	
+	if(errno != ECHILD)
+	{
+		unix_error("waitpid error");
+	}
+	
 	return;
 	
 }
 
 int main() {
-	char cmdline[MAXLINE];
-	
+
 	while(1) {
+		char cmdline[MAXLINE];
 		printf("> ");
 		fgets(cmdline, MAXLINE, stdin);
+		
+		// replace \t with spaces
+		for (int i = 0; cmdline[i] != '\0'; ++i) {
+			if (cmdline[i] == '\t')
+				cmdline[i] = ' ';
+		}
+		
 		if (feof(stdin))
 			exit(0);
 			
